4541d4b072f6238f4521cdaa4291e9f1745d4761
Rafael Darder <darder@gmail.com>
2023-03-12 18:20:05 -0300

    Export main public classes in the top level wiring package.

---
+++ src/wiring/__init__.py
@@ -0,0 +1,6 @@
+from .module import Module
+from .resource import Resource
+from .provider import Provider
+from .container import Container
+
+__all__ = ["Module", "Provider", "Container", "Resource"]
